1937e0ff42db872292009fabb1d4e8d09e8783c7
Dominik Strutz <kaaeveth@bofloos.de>
2020-10-10 19:00:46 +0200

    Zeichnen von Kreisen durch Doppelklick & Kreisbuffer zu Typ Shape geändert

--- src/DrawingCanvas.java
+++ src/DrawingCanvas.java
@@ -5,23 +5,23 @@ import java.awt.geom.Ellipse2D;
 
 public class DrawingCanvas extends Canvas implements MouseListener{
 	
-	private Ellipse2D[] _circles; //Alle Kreise auf dem Canvas (Buffer)
+	private Shape[] _circles; //Alle Kreise auf dem Canvas (Buffer)
 	private int _currentCircles;  //Anzahl aller momentanen Kreise
 	
 	public DrawingCanvas(int circles) {
 		super();
 		addMouseListener(this);
-		_circles = new Ellipse2D[circles]; // circle ist die maximale Anzahl an Kreisen
+		_circles = new Shape[circles]; // circles ist die maximale Anzahl an Kreisen
 		_currentCircles = 0;
 		setBackground(Color.DARK_GRAY);
 		
 	}
 	
 	//Fuegt einen Kreis ein, x/y - Mauskoordinaten / r - Radius
-	private void drawCircle(float x, float y, float r) {//Sind maximal viele Kreise im Buffer?
+	private void drawCircle(float x, float y, float r) {
 		//Sind maximal viele Kreise im Buffer?
 		if(_currentCircles == _circles.length) {  
-			_circles = new Ellipse2D[_circles.length]; // Buffer reseten
+			_circles = new Shape[_circles.length]; // Buffer reseten
 			_currentCircles = 0;
 		} else {
 			_circles[_currentCircles++] = new Ellipse2D.Float(x,y,r,r); //Neuen Kreis im Buffer
@@ -33,15 +33,18 @@ public class DrawingCanvas extends Canvas implements MouseListener{
 	public void paint(Graphics g) {
 		Graphics2D gd2 = (Graphics2D)g;
 		gd2.setColor(Color.WHITE); //Kreisfarbe
-		for(Ellipse2D cir : _circles) {
+		for(Shape cir : _circles) {
 			if(cir != null)
-				gd2.draw(cir);		
+				gd2.draw(cir);
 		}
 	}
 	
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		drawCircle(e.getX(), e.getY(), 30.0f);
+		if (e.getClickCount() == 2 && !e.isConsumed()) { //Doppelclick
+		     e.consume();
+		     drawCircle(e.getX(), e.getY(), 30.0f);
+		}
 	}
 
 	@Override
--- src/GUI.java
+++ src/GUI.java
@@ -1,6 +1,4 @@
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
