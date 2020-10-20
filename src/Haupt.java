54204e464dfbcd1132ae469881318f6c7931e5f9
Dominik Strutz <kaaeveth@bofloos.de>
2020-10-20 22:11:02 +0200

    Versionsnummer auf 1.1.1

--- src/Haupt.java
+++ src/Haupt.java
@@ -2,7 +2,7 @@ import java.awt.Dimension;
 
 public class Haupt {
 
-	final static String version = "1.0";
+	final static String version = "1.1.1";
 	final static Dimension windowSize = new Dimension(800, 500);
 	
 	public static void main(String[] args) {
